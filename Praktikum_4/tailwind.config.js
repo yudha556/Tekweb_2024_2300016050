bfce6232887e5e0287f6cee165b4f96547f23775
Yudhaaaaa <yudhapratama11005@gmail.com>
2024-10-30 23:23:05 +0700

    baru segini

---
+++ Praktikum_4/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html','./css/**/*.css'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'], 
+    },
+  },
+  plugins: [],
+}
+
